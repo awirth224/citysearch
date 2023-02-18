4916c825678fc4ab46a4f0f480d783f2cea29073
Matt Rowan <matt.rowan121@gmail.com>
2023-02-18 17:39:07 -0500

    Add first pass of populating dropdown display

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -22,30 +22,44 @@ class App extends Component<MyProps, MyState> {
 
   handleClick = (e: any) => {
     e.preventDefault()
-    searchFetch(this.state.city).then(data => {
-      console.log(data)
-      const allCities = data["_embedded"]["city:search-results"]
-      this.getOptions(allCities)
-      this.setState({cities: allCities})
-    })
+    // searchFetch(this.state.city).then(data => {
+    //   console.log(data)
+    //   const allCities = data["_embedded"]["city:search-results"]
+    //   this.getOptions(allCities)
+    //   this.setState({cities: allCities})
+    // })
   }
   
-  getOptions = (cities: []) => {
+  getUserOptions = (cities: []) => {
     const allResults: string[] = cities.map(city => city["matching_full_name"])
     console.log('ALL RESULTS:', allResults)
+    return allResults
   }
 
   handleChange = (e: any) => {
     // const {name, value} = e.target
+    searchFetch(this.state.city).then(data => {
+      console.log(data)
+      const allCities = data["_embedded"]["city:search-results"]
+      this.getUserOptions(allCities)
+      this.setState({cities: allCities})
+    })
     this.setState({ city: e.target.value })
   }
 
   render() {
+    // const lemons = this.getUserOptions(this.state.cities).map(lemon => {
+    //   return (
+    //     <option>{lemon}</option>
+    //   )
+    // })
+
     return (
       <main className='app'>
         <Header />
         <form>
-          <input type='search' name='city' value={this.state.city} placeholder='Search' onChange={(event) => this.handleChange(event)}/>
+          <input type='search' list='cityNames' autoComplete='off' name='city' value={this.state.city} placeholder='Search' onChange={(event) => this.handleChange(event)}/>
+          {/* <datalist id='cityNames'>{lemons}</datalist> */}
           <button onClick={(e) => this.handleClick(e)}>Search</button>
         </form>
       </main>
