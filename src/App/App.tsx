4855ed2d13ee801546f2a3b4dd5a16cc9172dbeb
Matt Rowan <matt.rowan121@gmail.com>
2023-02-18 16:18:19 -0500

    Add handle click, get options, and handle change methods

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header'
+import searchFetch from '../apicalls/allCitiesApiCall';
 
 type MyProps = {
 
@@ -19,10 +20,34 @@ class App extends Component<MyProps, MyState> {
     url: ''
   }
 
+  handleClick = (e: any) => {
+    e.preventDefault()
+    searchFetch(this.state.city).then(data => {
+      console.log(data)
+      const allCities = data["_embedded"]["city:search-results"]
+      this.getOptions(allCities)
+      this.setState({cities: allCities})
+    })
+  }
+  
+  getOptions = (cities: []) => {
+    const allResults: string[] = cities.map(city => city["matching_full_name"])
+    console.log('ALL RESULTS:', allResults)
+  }
+
+  handleChange = (e: any) => {
+    // const {name, value} = e.target
+    this.setState({ city: e.target.value })
+  }
+
   render() {
     return (
       <main className='app'>
         <Header />
+        <form>
+          <input type='search' name='city' value={this.state.city} placeholder='Search' onChange={(event) => this.handleChange(event)}/>
+          <button onClick={(e) => this.handleClick(e)}>Search</button>
+        </form>
       </main>
     )
   }
