d15676a3debaf1325accc382fae9ba247824e272
Angela Wirth <angiewirth92@gmail.com>
2023-02-19 14:08:41 -0500

    Move city logo png to src

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import './Header.css'
-import 'cityIcon.png'
+import '../cityIcon.png'
+import '../City.jpeg'
 
 const Header = () => {
   return (
     <header className='header'>
-      <div className='logo'><img src="cityIcon.png"/>CitySearch</div>
+      <div className='logo'><img src="../cityIcon.png"/>CitySearch</div>
     </header>
   )
 }
