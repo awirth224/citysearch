5c0bf330de56893ae94b6804853df87c09de8495
Keenan Southall <keenan.southall@yahoo.com>
2023-02-20 11:37:48 -0700

    Buildout functional component

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+
+type CardProps = {
+    theCityInfo: any;
+}
+
+const Card: React.FC<CardProps> = ({ theCityInfo }) => {
+    const name: string = theCityInfo.name
+    const image: string = theCityInfo.image
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <img src={image} alt={name} />
+        </div>
+    )
+}
+
+export default Card
