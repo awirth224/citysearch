e1829f8bd08ace1cca4b2e11d16e11a213dbfb1b
Keenan Southall <keenan.southall@yahoo.com>
2023-02-20 21:07:06 -0700

    Implement card flip functionality

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
-import './Card.css'
+import './Card.css';
 
 type CardProps = {
 	theCityInfo: any;
-
 }
 
 type CardState = {
-	clicked: boolean;
+	frontClicked: boolean;
+	backClicked: boolean;
 }
 
 class Card extends Component<CardProps, CardState> {
 
 	state: CardState = {
-		clicked: false
+		frontClicked: false,
+		backClicked: true
 	}
 
-	// false indicates the front is showing 
-	// true indicates the back is showing
+	clickFrontButton = () => {
+		this.setState({ frontClicked: true, backClicked: false })
+	}
+
+	clickBackButton = () => {
+		this.setState({ backClicked: true, frontClicked: false })
+	}
+
+
 
 	render() {
 
@@ -33,16 +41,20 @@ class Card extends Component<CardProps, CardState> {
 		const environmentalQuality: number = this.props.theCityInfo.environmentalQuality;
 		const safety: number = this.props.theCityInfo.safety;
 
-		return (
-			<section className="card-content" id='flip-card' >
+		let cardDisplay: any;
+
+		if (this.state.backClicked) {
+			cardDisplay =
 				<section className="card-front">
 					<img src={image} alt={name} />
 					<h2>City:  {name}</h2>
 					<h2> Population:  {population}</h2>
-					<button>front</button>
+					<button onClick={() => this.clickFrontButton()}>City Info</button>
 				</section>
-				<section className="card-back hidden" >
-					<h2> All Ratings Out Of 10 </h2>
+		} else if (this.state.frontClicked) {
+			cardDisplay =
+				<section className="card-back" >
+					<h2> All {name} Ratings Out Of 10 </h2>
 					<section className="info-display">
 						<h2> Cost of living: </h2>
 						<div className="info-wrapper">{costOfLiving}</div>
@@ -75,62 +87,16 @@ class Card extends Component<CardProps, CardState> {
 						<h2> HealthCare: </h2>
 						<div className="info-wrapper">	{healthCare} </div>
 					</section>
-					<button>back</button>
+					<button onClick={() => this.clickBackButton()}>Back</button>
 				</section>
+		}
 
+		return (
+			<section className="card-content" id='flip-card' >
+				{cardDisplay}
 			</section>
-
-
 		)
 	}
-
-	// return (
-	// 	<section className="card-content">
-	// 		<section className="card-front ">
-	// 			<img src={image} alt={name} />
-	// 			<h2>City:  {name}</h2>
-	// 			<h2> Population:  {population}</h2>
-	// 			<button>front</button>
-	// 		</section>
-	// 		<section className="card-back" >
-	// 			<h2> All Ratings Out Of 10 </h2>
-	// 			<section className="info-display">
-	// 				<h2> Cost of living: </h2>
-	// 				<div className="info-wrapper">{costOfLiving}</div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Environmental Quality: </h2>
-	// 				<div className="info-wrapper">{environmentalQuality} </div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Safety: </h2>
-	// 				<div className="info-wrapper"> {safety} </div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Economy: </h2>
-	// 				<div className="info-wrapper">{economy}</div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Commute: </h2>
-	// 				<div className="info-wrapper">{commute} </div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Housing: </h2>
-	// 				<div className="info-wrapper">{housing}</div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> Education: </h2>
-	// 				<div className="info-wrapper"> {education} </div>
-	// 			</section>
-	// 			<section className="info-display">
-	// 				<h2> HealthCare: </h2>
-	// 				<div className="info-wrapper">	{healthCare} </div>
-	// 			</section>
-	// 			<button>back</button>
-	// 		</section>
-
-	// 	</section>
-	// )
 }
 
 export default Card
