639f3b851e4762eb7a775761c6f84e278ee6eefc
Keenan Southall <keenan.southall@yahoo.com>
2023-02-21 21:07:40 -0700

    Add JSX and props

--- src/Card/FrontCard.tsx
+++ src/Card/FrontCard.tsx
@@ -2,12 +2,22 @@ import React, { FC } from 'react'
 
 
 interface FrontProps {
-
+    cityName: string;
+    cityPopulation: number;
+    cityImage: string;
+    clickFrontButton: any;
 }
 
 const FrontCard: FC<FrontProps> = props => {
     return (
-        <div></div>
+        <div>
+            <section className="card-front">
+                <img src={props.cityImage} alt={'test'} />
+                <h2>City:  {props.cityName}</h2>
+                <h2> Population:  {props.cityPopulation.toLocaleString('en-US')}</h2>
+                <button onClick={() => props.clickFrontButton()}>City Info</button>
+            </section>
+        </div>
     )
 }
 
