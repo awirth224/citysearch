0baedce84cde57fb4170f707809167709b6ea9a4
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-02-24 16:14:11 -0500

    Start the testing files

--- cypress/e2e/citySearch.cy.ts
+++ cypress/e2e/citySearch.cy.ts
@@ -1,5 +1,30 @@
-// describe('template spec', () => {
-//   it('Should visit our web site', () => {
-//     cy.visit('http://localhost:3000/')
-//   })
-// })
+describe('City Search site', () => {
+  beforeEach(() => {
+    cy.intercept()
+    
+    
+    
+    cy.visit('http://localhost:3000/')
+    
+  })
+
+  it('Should display a title', function() {
+    cy.get('.logo').should('contain', 'CitySearch')
+  })
+
+})
+
+
+/*
+user - goes to page
+should see a form
+i should be able o type into an input box
+should be able to click submit
+should be able to see card componets
+image visable
+stats visible 
+click home to go back to home
+... 
+
+* */
+
