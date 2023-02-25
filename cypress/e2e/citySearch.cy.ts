9008cfed4f182f02d643b7f9d4aa48b95c39a5cc
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-02-24 20:23:18 -0500

    Add testing for user interaction

--- cypress/e2e/citySearch.cy.ts
+++ cypress/e2e/citySearch.cy.ts
@@ -1,43 +1,68 @@
 describe('City Search site', () => {
-  beforeEach(() => {
-    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',
-    { fixture: 'example.json' } )
-
-    cy.visit('http://localhost:3000/')
-    
-  })
 
   it('Should display a title', function() {
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',{ fixture: 'example.json' } )
+    cy.visit('http://localhost:3000/')
     cy.get('.logo').should('contain', 'CitySearch')
   })
   
   it('Should have a form visible', function() {
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',{ fixture: 'example.json' } )
+    cy.visit('http://localhost:3000/')
     cy.get('form').should('be.visible')
   })
   
   it('Should allow a user to type and submit a form', function(){
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',{ fixture: 'example.json' } )
+    cy.visit('http://localhost:3000/')
     cy.get('form').within(() => {
     cy.get('input[name="homeCity"]').type('Aarhus, Denmark').should('have.value', 'Aarhus, Denmark')
     cy.get('input[name="desiredCity"]').type('Almaty, Kazakhstan').should('have.value', 'Almaty, Kazakhstan')
     cy.get('button').click()
     })
     cy.url().should('eq','http://localhost:3000/cities') 
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:almaty/scores/',{ fixture:'desiredCityInfo'} )
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:aarhus/scores/',{ fixture:'homeCityInfo'})
   })
-  
+  it('Should display two  cities information', function(){
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',{ fixture: 'example.json' } )
+    cy.visit('http://localhost:3000/')
+    cy.get('form').within(() => {
+    cy.get('input[name="homeCity"]').type('Aarhus, Denmark').should('have.value', 'Aarhus, Denmark')
+    cy.get('input[name="desiredCity"]').type('Almaty, Kazakhstan').should('have.value', 'Almaty, Kazakhstan')
+    cy.get('button').click()
+    })
+    cy.url().should('eq','http://localhost:3000/cities') 
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:almaty/scores/',{ fixture:'desiredCityInfo'} )
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:aarhus/scores/',{ fixture:'homeCityInfo'})
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:almaty/images/',{fixture:'desiredImage'})
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:aarhus/images/',{fixture:'homeImage'})
+
+    cy.get('.card-content').should('be.visible')
+    cy.get('.card-front').children('img').should('have.length', 2)
+    cy.get('.info-display').children('h2').should('have.length',16) 
+  })
+   
+  it('Should allow a user to go back to the home page when the home button is clicked',function(){
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/',{ fixture: 'example.json' } )
+    cy.visit('http://localhost:3000/')
+    cy.get('form').within(() => {
+    cy.get('input[name="homeCity"]').type('Aarhus, Denmark').should('have.value', 'Aarhus, Denmark')
+    cy.get('input[name="desiredCity"]').type('Almaty, Kazakhstan').should('have.value', 'Almaty, Kazakhstan')
+    cy.get('button').click()
+    })
+    cy.url().should('eq','http://localhost:3000/cities') 
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:almaty/scores/',{ fixture:'desiredCityInfo'} )
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:aarhus/scores/',{ fixture:'homeCityInfo'})
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:almaty/images/',{fixture:'desiredImage'})
+    cy.intercept('GET','https://api.teleport.org/api/urban_areas/slug:aarhus/images/',{fixture:'homeImage'})
+    cy.get('.card-content').should('be.visible')
+    cy.get('.card-front').children('img').should('have.length', 2)
+    cy.get('.info-display').children('h2').should('have.length',16) 
+    cy.get('.home-btn').click()
+    cy.url().should('eq','http://localhost:3000/')
+  })
+    
 
 })
-
-
-/*
-user - goes to page
-should see a form
-i should be able o type into an input box
-should be able to click submit
-should be able to see card componets
-image visable
-stats visible 
-click home to go back to home
-... 
-
-* */
 
