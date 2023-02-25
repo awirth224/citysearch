19ac6c34a7c0434fadccb665dccd98808a226141
Keenan Southall <keenan.southall@yahoo.com>
2023-02-24 18:21:27 -0700

    WIP conditional

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -13,8 +13,8 @@ type MyState = {
     homeCity: string;
     desiredCity: string;
     disabled: boolean;
-    homeFound: boolean;
-    desiredFound: boolean;
+    homeCityFound: boolean;
+    desiredCityFound: boolean;
 }
 
 class Form extends Component<MyProps, MyState> {
@@ -23,61 +23,97 @@ class Form extends Component<MyProps, MyState> {
         homeCity: '',
         desiredCity: '',
         disabled: true,
-        homeFound: false,
-        desiredFound: false
+        homeCityFound: false,
+        desiredCityFound: false
     }
 
-    //isdisabled 
+    // handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    //     const { name, value } = e.currentTarget;
+
+    //     // let homeCityFound: any;
+    //     // let desiredCityFound: any;
+
+    //     // if (name === 'homeCity') {
+    //     //     homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)
+    //     // }
+
+    //     // if (name === 'desiredCity') {
+    //     //     desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)
+    //     // }
+
+    //     // if (homeCityFound && desiredCityFound) {
+    //     //     this.setState({ disabled: false })
+    //     // }
+
+    //     this.setState(prevState => ({
+    //         ...prevState,
+    //         [name]: value,
+    //     }))
+
+    //     const homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.homeCity)
+    //     const desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.desiredCity)
+
+    //     console.log('homeCityFound', homeCityFound)
+    //     console.log('desiredCityFound', desiredCityFound)
+
+    //     if (homeCityFound === undefined && desiredCityFound === undefined) {
+    //         this.setState({ disabled: true })
+    //     } else if (homeCityFound === undefined && desiredCityFound) {
+    //         this.setState({ disabled: true })
+    //     } else if (homeCityFound && desiredCityFound === undefined) {
+    //         this.setState({ disabled: true })
+    //     } else {
+    //         this.setState({ disabled: false })
+    //     }
+
+    //     //this.checkInputs()
+    // }
 
     handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
         const { name, value } = e.currentTarget;
-
-        // let homeCityFound: any;
-        // let desiredCityFound: any;
-
-        // if (name === 'homeCity') {
-        //     homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)
-        // }
-
-        // if (name === 'desiredCity') {
-        //     desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)
-        // }
-
-        // if (homeCityFound && desiredCityFound) {
-        //     this.setState({ disabled: false })
-        // }
 
         this.setState(prevState => ({
             ...prevState,
             [name]: value,
         }))
 
-        const homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.homeCity)
-        const desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.desiredCity)
+        let home: any;
+        let desired: any;
 
-        console.log('homeCityFound', homeCityFound)
-        console.log('desiredCityFound', desiredCityFound)
 
-        if (homeCityFound === undefined && desiredCityFound === undefined) {
-            this.setState({ disabled: true })
-        } else if (homeCityFound === undefined && desiredCityFound) {
-            this.setState({ disabled: true })
-        } else if (homeCityFound && desiredCityFound === undefined) {
-            this.setState({ disabled: true })
-        } else {
+        if (name === 'homeCity') {
+            if (this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)) {
+                this.setState({ homeCityFound: true })
+                //home = true
+            }
+        }
+
+        if (name === 'desiredCity') {
+            if (this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === value)) {
+                this.setState({ desiredCityFound: true })
+                //desired = true
+            }
+        }
+
+        console.log('home state', this.state.homeCityFound)
+        console.log('desired state', this.state.desiredCityFound)
+
+        if (this.state.homeCityFound && this.state.desiredCityFound) {
             this.setState({ disabled: false })
+        } else {
+            this.setState({ disabled: true })
         }
 
-        //this.checkInputs()
-    }
+        // if (home && desired) {
+        //     this.setState({ disabled: false })
+        // } else {
+        //     this.setState({ disabled: true })
+        // }
 
-    checkInputs = () => {
-        const homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.homeCity)
-        const desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.desiredCity)
+        // console.log('homeCityFound', this.state.homeCityFound)
+        // console.log('desiredCityFound', this.state.desiredCityFound)
 
-        if (homeCityFound && desiredCityFound) {
-            this.setState({ disabled: false, homeFound: true, desiredFound: true })
-        }
+
     }
 
     createDatalist = () => {
@@ -86,20 +122,7 @@ class Form extends Component<MyProps, MyState> {
     }
 
     handleClick = () => {
-
         this.props.handleCallback(this.state.homeCity, this.state.desiredCity)
-
-        // const homeCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.homeCity)
-        // const desiredCityFound = this.props.urbanAreas.find((item: { href: string, fullName: string }) => item.fullName === this.state.desiredCity)
-
-
-        // if (homeCityFound !== undefined && desiredCityFound !== undefined) {
-        //     this.setState({ endPoint: '/cities' })
-
-        // } else {
-        //     alert(':/')
-        // }
-
     }
 
     render() {
@@ -108,13 +131,13 @@ class Form extends Component<MyProps, MyState> {
                 <form>
                     <div className='input-container'>
                         <label>Starting City</label>
-                        <input type='search' list='listOne' autoComplete='off' name='homeCity' value={this.state.homeCity} placeholder='Enter your current city' onInput={(event) => this.handleChange(event)} required />
+                        <input type='search' list='listOne' autoComplete='off' name='homeCity' value={this.state.homeCity} placeholder='Enter your current city' onChange={(event) => this.handleChange(event)} required />
                         <datalist id='listOne'>{this.createDatalist()}</datalist>
                     </div>
                     {!this.props.homeUrbanArea && <h2>Please enter a valid city</h2>}
                     <div className='input-container'>
                         <label>Desired City</label>
-                        <input type='search' list='listTwo' autoComplete='off' name='desiredCity' value={this.state.desiredCity} placeholder='Enter your desired city' onInput={(event) => this.handleChange(event)} required />
+                        <input type='search' list='listTwo' autoComplete='off' name='desiredCity' value={this.state.desiredCity} placeholder='Enter your desired city' onChange={(event) => this.handleChange(event)} required />
                         <datalist id='listTwo'>{this.createDatalist()}</datalist>
                     </div>
                     {!this.props.desiredUrbanArea && <h2>Please enter a valid city</h2>}
