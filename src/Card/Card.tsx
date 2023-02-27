750e6fa7ca8a6435b7dd9700626e1f701e356d45
Keenan Southall <keenan.southall@yahoo.com>
2023-02-26 20:42:51 -0700

    Add error state for conditional render

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -5,25 +5,25 @@ import BackCard from "./BackCard";
 import { getSpecifiedInfo } from '../apicalls/allCitiesApiCall';
 
 type MyProps = {
-    urbanAreas: any;
+	urbanAreas: any;
 	citySlug: string;
 }
 
 type MyState = {
-    cityImage: string,
+	cityImage: string,
 	cityScores: [],
 	cityName: string,
-	error: string
+	errorMessage: string
 }
 
 class Card extends Component<MyProps, MyState> {
 
-    state: MyState = {
-        cityImage: '',
+	state: MyState = {
+		cityImage: '',
 		cityScores: [],
 		cityName: '',
-		error: '',
-    }
+		errorMessage: '',
+	}
 
 	componentDidMount(): void {
 		this.getCityDeets(this.props.citySlug)
@@ -37,37 +37,41 @@ class Card extends Component<MyProps, MyState> {
 
 	getCityName = (citySlug: string) => {
 		getSpecifiedInfo(citySlug)
-		.then(data => {
-			this.setState({ cityName: data['full_name'] })
-		})
+			.then(data => {
+				this.setState({ cityName: data['full_name'] })
+			})
+			.catch(error => {
+				this.setState({ errorMessage: error })
+			})
 	}
 
 	getCityScores = (citySlug: string, endpoint: string) => {
 		getSpecifiedInfo(citySlug, endpoint)
-		  .then(data => {
-			const newScores = data.categories.reduce((acc: any, curr: any) => {
-			  acc[curr.name] = curr.score_out_of_10
-			  return acc
-			}, {})
-			
-			this.setState({ cityScores: newScores })
-		  })
+			.then(data => {
+				const newScores = data.categories.reduce((acc: any, curr: any) => {
+					acc[curr.name] = curr.score_out_of_10
+					return acc
+				}, {})
+
+				this.setState({ cityScores: newScores })
+			})
 	}
 
 	getCityImages = (citySlug: string, endpoint: string) => {
 		getSpecifiedInfo(citySlug, endpoint)
-		  .then(data => {
-			const image = data.photos[0].image.web
-			
-			this.setState({ cityImage: image })
-		  })
+			.then(data => {
+				const image = data.photos[0].image.web
+
+				this.setState({ cityImage: image })
+			})
 	}
 
 	render() {
 		return (
 			<section className="card-content" id='flip-card' >
- 				<FrontCard cityName={this.state.cityName} cityImage={this.state.cityImage} />
-				<BackCard cityInfo={this.state.cityScores} />
+				{!this.state.errorMessage && <FrontCard cityName={this.state.cityName} cityImage={this.state.cityImage} />}
+				{!this.state.errorMessage && <BackCard cityInfo={this.state.cityScores} />}
+				{this.state.errorMessage && <img src='https://i.imgflip.com/3811ub.jpg' alt='error message' />}
 			</section>
 		)
 	}
